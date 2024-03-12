900ae39c2c21cc0b36009e502e30e85b11975fd0
artkl12 <arta.klauze@gmail.com>
2024-03-12 14:18:34 +0200

    message

---
+++ src/Header.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import "./header.css"
+
+function Header() {
+  return (
+    <>
+    <section className='header'>
+      <header>
+        <img src="https://th.bing.com/th/id/R.3fd09c690db35e1a30752f91a16aaddf?rik=isIEzIwrq5%2fzvQ&pid=ImgRaw&r=0" alt="lemon" />
+      </header>
+      <nav>
+        <ul>
+          <li>
+            <a href="#">about</a>
+            <a href="#">about</a>
+            <a href="#">about</a>
+            <a href="#">about</a>
+          </li>
+        </ul>
+      </nav>
+      </section>
+    </>
+  )
+}
+
+export default Header
---
+++ src/Main.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Main() {
+  return (
+    <>
+      <h1>Main section</h1>
+ 
+    </>
+  );
+}
+
+export default Main;
